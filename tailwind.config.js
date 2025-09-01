585fac0395caaa0c1972e64d892b803c6c89f3fc
Neha Haneef <nehahaneef203@gmail.com>
2025-09-01 16:07:02 +0500

    l

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,10 @@ module.exports = {
   ],
   theme: {
     screens: {
-      sm: "640px",   // Small devices
-      md: "768px",   // Tablets
-      lg: "1024px",  // Laptops
-      xl: "1280px",  // Desktops
-     
-      
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
     extend: {
       fontFamily: {
@@ -35,10 +33,41 @@ module.exports = {
             filter: 'blur(12px)',
           },
         },
+        photon: {
+          "0%, 100%": { 
+            transform: "translate(0, 0) scale(1)",
+            opacity: "0.2" 
+          },
+          "25%": { 
+            transform: "translate(var(--tx, 0), var(--ty, 0)) scale(1.1)",
+            opacity: "0.8" 
+          },
+          "50%": { 
+            transform: "translate(0, 0) scale(0.9)",
+            opacity: "0.4" 
+          },
+          "75%": { 
+            transform: "translate(calc(-1 * var(--tx, 0)), calc(-1 * var(--ty, 0))) scale(1.05)",
+            opacity: "0.7" 
+          },
+        },
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
       animation: {
         shine: 'shine 5s linear infinite',
         glow: 'glow 3s ease-in-out infinite',
+        photon: "photon 1s linear infinite",
+        beam: "beam 15s ease-in-out infinite alternate",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
     },
   },
